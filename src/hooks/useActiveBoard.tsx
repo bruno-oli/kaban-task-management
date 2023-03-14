9967a292fd8dadaf00aa8e1dce7edf3de9eb5fc1
bruno-oli <brunomaz177@gmail.com>
2023-03-14 20:53:12 -0300

    fix: fix useActiveBoard problem

--- src/hooks/useActiveBoard.tsx
+++ src/hooks/useActiveBoard.tsx
@@ -1,9 +1,32 @@
 import React, { useContext } from "react";
-import { BoardsContext } from "../contexts/BoardsContext";
+import { BoardsContext, IBoard } from "../contexts/BoardsContext";
+
+interface IProps {
+  activeBoard: IBoard | undefined;
+  setActiveBoard: (newState: number) => void;
+}
 
 const useActiveBoard = () => {
-  const { activeBoardId, boards } = useContext(BoardsContext);
-  return boards.find((i) => i.id === activeBoardId)
+  const { boards, setBoards } = useContext(BoardsContext);
+  const activeBoard = boards.find((i) => {
+    return i.active;
+  });
+  const setActiveBoard = (id: number) => {
+    const boardsClone = [...boards];
+    boardsClone.forEach((i) => {
+      if (i.id === id) {
+        i.active = true;
+      } else {
+        i.active = false;
+      }
+    });
+    setBoards(boardsClone)
+  };
+  const values: IProps = {
+    activeBoard,
+    setActiveBoard,
+  };
+  return values;
 };
 
 export default useActiveBoard;
